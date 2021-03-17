6d7943a9871fa7a9d11cc94cf411d6c4c227c2a2
aivanov <a.ivanov@aladdin-rd.ru>
2021-03-17 15:58:36 +0300

    Addresses update

--- src/modules/accounts/features/AccountsForm/components/AddressesTab/index.tsx
+++ src/modules/accounts/features/AccountsForm/components/AddressesTab/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { TAccount } from 'modules/accounts/config/types';
+import Context from 'store';
+
+import AddressesList from '../AddressesList';
 
 const AddressesTab: React.FC = () => {
+  const { formView } = useContext(Context);
+  const account = formView.state.data as TAccount;
+
   return (
     <>
       <h2>Адресная информация</h2>
+      <AddressesList addresses={account.addresses} />
     </>
   );
 };

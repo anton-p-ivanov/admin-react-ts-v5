49a4ea8e22f107e0049fef285cb9a025c099bb1b
aivanov <a.ivanov@aladdin-rd.ru>
2021-03-30 13:50:24 +0300

    Refactoring

--- src/modules/fields/features/FieldsModal/hooks/useHandlers.ts
+++ src/modules/fields/features/FieldsModal/hooks/useHandlers.ts
@@ -22,27 +22,36 @@ const useHandlers = (): TUseHandlers => {
 
   const confirmHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
+
+    API.request({
+      method: 'POST',
+      url: `/fields/validate`,
+      data: modal.state.data,
+    })
+      .then(onValidateSucceed)
+      .catch(onValidatedFailed);
+  };
+
+  const onValidateSucceed = () => {
     const form = formView.state.data;
     const fields = form.fields as TField[];
     const field = modal.state.data as TField;
 
-    // validate field before state update
-    API.request({ method: 'POST', url: `/fields/validate`, data: field })
-      .then(() => {
-        const index = fields.findIndex((item: TField) => item.code === field.code);
-        if (index > -1) {
-          fields[index] = field;
-        } else {
-          fields.push(field);
-        }
-        formView.update({ ...formView.state, data: { ...form, fields } });
-        modal.reset();
-      })
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      .catch((response: any) => {
-        const errors = response.response ? response.response.data : {};
-        modal.update({ ...modal.state, errors, isValid: false });
-      });
+    const index = fields.findIndex((item: TField) => item.code === field.code);
+    if (index > -1) {
+      fields[index] = field;
+    } else {
+      fields.push(field);
+    }
+
+    formView.update({ ...formView.state, data: { ...form, fields } });
+    modal.reset();
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onValidatedFailed = (response: any) => {
+    const errors = response.response ? response.response.data : {};
+    modal.update({ ...modal.state, errors, isValid: false });
   };
 
   return {

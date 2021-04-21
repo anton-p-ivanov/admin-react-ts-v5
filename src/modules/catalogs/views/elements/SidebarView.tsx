4d60b58c0d77d7103cf5cd0f3192192b47090c84
aivanov <a.ivanov@aladdin-rd.ru>
2021-04-21 12:20:14 +0300

    Sidebar update

--- src/modules/catalogs/views/elements/SidebarView.tsx
+++ src/modules/catalogs/views/elements/SidebarView.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { NavSection } from 'layouts/default/components';
+import { NavItem, NavSection } from 'layouts/default/components';
+import API, { TLookupResult } from 'utils/api';
 
 const SidebarView: React.FC = () => {
+  const [items, setItems] = useState<TLookupResult[]>([]);
+
+  useEffect(() => {
+    API.lookup(`/catalogs/types/lookup`).then((response) => {
+      setItems(response);
+    });
+  }, []);
+
   return (
     <NavSection title={`Справочники`} icon={`layers`}>
-      Выберите один из справочников справа
+      {items.map((item) => {
+        return <NavItem key={item.value} path={`/catalogs/${item.value}/list`} title={item.label} />;
+      })}
     </NavSection>
   );
 };
